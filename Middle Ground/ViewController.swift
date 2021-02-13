a72367a9a5def84e5800dd1abcce6e0e7b3f3386
kaylawang05 <kayla.y.wang@gmail.com>
2021-02-13 17:48:10 -0500

    first commit

---
+++ Middle Ground/ViewController.swift
@@ -0,0 +1,42 @@
+//
+//  ViewController.swift
+//  Middle Ground
+//
+//  Created by Kayla Wang on 2/13/21.
+//
+
+import UIKit
+import AVFoundation
+import AVKit
+
+class ViewController: UIViewController {
+
+    let playButton = UIButton()
+    @objc func playVideo(_ sender: UIButton) {
+        guard let url = URL(string: "https://devstreaming-cdn.apple.com/videos/streaming/examples/bipbop_adv_example_hevc/master.m3u8") else {
+            return
+        }
+        // Create an AVPlayer, passing it the HTTP Live Streaming URL.
+        let player = AVPlayer(url: url)
+
+        // Create a new AVPlayerViewController and pass it a reference to the player.
+        let controller = AVPlayerViewController()
+        controller.player = player
+
+        // Modally present the player and call the player's play() method when complete.
+        present(controller, animated: true) {
+            player.play()
+        }
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.view.addSubview(playButton)
+        playButton.addTarget(self, action: #selector(playVideo(_:)), for: .touchUpInside)
+        // Do any additional setup after loading the view.
+    }
+
+    
+    
+}
+
